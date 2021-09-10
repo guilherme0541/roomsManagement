b0e5b6859aae670517151bd21badb5e55e8a146f
Guilherme Silva <guilherme0541@gmail.com>
2021-09-10 18:30:55 -0300

    First commit

---
+++ src/test/java/com/guilherme/silva/salareuniao/SalareuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.silva.salareuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalareuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
